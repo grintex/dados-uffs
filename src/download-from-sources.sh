afe7904dd209cb1d19bf69669dc804b41dd5d098
Fernando Bevilacqua <dovyski@gmail.com>
2020-08-29 17:31:38 -0300

    Cria pastas para organizar info de professores

--- src/download-from-sources.sh
+++ src/download-from-sources.sh
@@ -9,6 +9,17 @@ for row in $(cat $JSON_DATA_DIR/sources/cursos.json | jq -r '.[] | @base64'); do
      echo ${row} | base64 --decode | jq -r ${1}
     }
 
-    echo $(_jq '.id') $(_jq '.link')
+    echo $(_jq '.link')
+    
+    link=$(_jq '.link')
+    remove="https://www.uffs.edu.br/"
+    link_structure=${link/$remove/}
+    output_path="$JSON_DATA_DIR/professores/$link_structure"
+
+    mkdir -p $output_path
     #scrapy runspider $(SRC_DIR)/download-source-programs.py -o $(JSON_DATA_DIR)/sources/cursos.json -a parameter1=value1 -a parameter2=value2
 done
+
+# Fix some inconsistencies
+mv $JSON_DATA_DIR/professores/campi $JSON_DATA_DIR/professores/posgraduacao
+mv $JSON_DATA_DIR/professores/graduacao/campus-cerro-largo $JSON_DATA_DIR/professores/graduacao/cerro-largo
